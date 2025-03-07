2711e6a7ebf5531bcbddd128c6b6d94de0168277
teran1416 <134824274+teran1416@users.noreply.github.com>
2025-03-06 20:19:20 -0500

    Add files via upload

--- PreparacionParcial.py
+++ PreparacionParcial.py
@@ -1,38 +1,59 @@
 from functools import reduce
 
-#lector de datos de un csv que funciona como un generador para un evaluador perezoso:
+# Lector de datos de un csv que funciona como un generador para un evaluador perezoso:
 def loadData(filePath):
-    with open(filePath,mode="r",encoding="utf-8") as file:
-        headers = file.readline().strip().split(",") #leer encabezado como las claves del diccionario
+    with open(filePath, mode="r", encoding="utf-8") as file:
+        headers = file.readline().strip().split(",")  # Leer encabezado como las claves del diccionario
         for line in file:
             values = line.strip().split(",")
             record = dict(zip(headers, values))
             yield record
 
-def USD_to_COP(x):
-    pass
+# Convertir salario de USD a COP
+def USD_to_COP(registro):
+    registro["Salario"] = int(registro["Salario"]) * 4100  # Convertir el salario a COP
+    return registro  
+
+
 def filter7Mill():
     pass
-def SumAll():
-    pass
+
+
+def SumAll(x,y):
+   pass
 
 dataStream = loadData("dataset.csv")
 
-#consumir tres registros y mostrarlos por pantalla:
+# Consumir tres registros y mostrarlos por pantalla:
 datalocal = []
-datalocal.append(next(dataStream))
-datalocal.append(next(dataStream))
-datalocal.append(next(dataStream))
+#filtar el stream hasta que no haya nada más: 
+try:
+    for __ in range(1000):
+        datalocal.append(next(dataStream))
+except StopIteration:
+    print("no hay más datos")
+
+
+# datalocal.append(next(dataStream))
+# datalocal.append(next(dataStream))
+# datalocal.append(next(dataStream))
 
 print(datalocal)
 
-#Convertir campo "salario de USD to COP mapeando una funcion de conversion de divisas:"
-#{'ID': '1' 'Nombre': 'Ana' ''}
-listaSalarioPesos= list(map(USD_to_COP,datalocal))
+# Convertir el campo "Salario" de USD a COP mapeando una función de conversión de divisas:
+listaSalarioPesos = list(map(USD_to_COP, datalocal))
+print(listaSalarioPesos)
 
-#Filtrar aquellos que tengan un salario mayor a $Cop 7`000.000
-listaMayores = list(filter(filter7Mill, listaSalarioPesos))
+# Filtrar aquellos que tengan un salario mayor a 7,000,000 COP
+listaMayores = list(filter(lambda e: int(e["Salario"]) > 7000000, listaSalarioPesos))
+print("\nTodos los usarios con salarios mayores a cop 7.000.000")
+print(listaMayores)
 
-#Sumar el total de salarios de esas personas con reduce:
-sumaTotal = reduce(SumAll, listaMayores)
+# Sumar el total de salarios de esas personas con reduce
+print("\Suma del salario de esos empleados")
+sumaTotal = reduce(lambda acum, e: acum + int(e["Salario"]), listaMayores, 0)
+
+print(f"Todos ganan: {sumaTotal}")
+
+
 
